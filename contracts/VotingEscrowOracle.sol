cab4abfa89145ecbfbb5e218c8cf37b08634fe36
Edward Amor <edward.amor3@gmail.com>
2022-02-25 09:42:42 +0000

    feat: add genereate_eth_get_proof_params fn
    Easy to query and simplifies the proof generation for clients

--- contracts/VotingEscrowOracle.sol
+++ contracts/VotingEscrowOracle.sol
@@ -6,8 +6,24 @@ interface AnyCallProxy {
 }
 
 contract VotingEscrowOracle {
+    struct LockedBalance {
+        int128 amount;
+        uint256 end;
+    }
+
+    struct Point {
+        int128 bias;
+        int128 slope;
+        uint256 ts;
+        uint256 blk;
+    }
+
+    /// Address of the voting escrow contract on Ethereum
+    address constant VOTING_ESCROW = 0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2;
     /// Hash of the block header for the Ethereum genesis block
     bytes32 constant GENESIS_BLOCKHASH = 0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3;
+    /// Week in seconds
+    uint256 constant WEEK = 1 weeks;
 
     /// Address of the AnyCallProxy for the chain this contract is deployed on
     address public immutable ANYCALL;
@@ -35,6 +51,58 @@ contract VotingEscrowOracle {
         emit TransferOwnership(address(0), msg.sender);
 
         ANYCALL = _anycall;
+    }
+
+    /**
+      * @notice Generate the arguments required for the `eth_getProofs` RPC call
+      * @param _user The account the storage proof will be generated for
+      * @param _global_epoch The value returned from `VotingEscrow.epoch()`
+      * @param _user_epoch The value returned from `VotingEscrow.user_point_epoch(address)`
+      * @param _last_point_ts The timestamp of the last global point returned from `VotingEscrow.point_history(uint256)`
+      */
+    function generate_eth_get_proof_params(
+        address _user,
+        uint256 _global_epoch,
+        uint256 _user_epoch,
+        uint256 _last_point_ts,
+    ) external view returns(address account, uint256[20] memory positions){
+        account = VOTING_ESCROW;
+
+        // `VotingEscrow.epoch()`
+        positions[0] = 3;
+
+        // `VotingEscrow.point_history(uint256)`
+        uint256 point_history_pos = uint256(keccak256(abi.encode(uint256(keccak256(abi.encode(4))) + _global_epoch)));
+        positions[1] = point_history_pos; // bias
+        positions[2] = point_history_pos + 1; // slope
+        positions[3] = point_history_pos + 2; // ts
+        positions[4] = point_history_pos + 3; // blk
+
+        // `VotingEscrow.user_point_epoch(address)`
+        positions[5] = uint256(keccak256(abi.encode(6, _user)));
+
+        // `VotingEscrow.user_point_history(address,uint256)`
+        uint256 user_point_history_pos = uint256(keccak256(abi.encode(uint256(keccak256(abi.encode(keccak256(abi.encode(5, _user))))) + _user_epoch)));
+        positions[6] = user_point_history_pos; // bias
+        positions[7] = user_point_history_pos + 1; // bias
+        positions[8] = user_point_history_pos + 2; // bias
+        positions[9] = user_point_history_pos + 3; // bias
+
+        // `VotingEscrow.locked(address)`
+        uint256 locked_pos = uint256(keccak256(abi.encode(keccak256(abi.encode(2, _user)))));
+        positions[10] = locked_pos;
+        positions[11] = locked_pos + 1;
+
+        // `VotingEscrow.slope_changes(uint256)`
+        uint256 start_time = (_last_point_ts / WEEK) * WEEK;
+        positions[12] = uint256(keccak256(abi.encode(7, start_time)));
+        positions[13] = uint256(keccak256(abi.encode(7, start_time + WEEK)));
+        positions[14] = uint256(keccak256(abi.encode(7, start_time + WEEK * 2)));
+        positions[15] = uint256(keccak256(abi.encode(7, start_time + WEEK * 3)));
+        positions[16] = uint256(keccak256(abi.encode(7, start_time + WEEK * 4)));
+        positions[17] = uint256(keccak256(abi.encode(7, start_time + WEEK * 5)));
+        positions[18] = uint256(keccak256(abi.encode(7, start_time + WEEK * 6)));
+        positions[19] = uint256(keccak256(abi.encode(7, start_time + WEEK * 7)));
     }
 
     /**
