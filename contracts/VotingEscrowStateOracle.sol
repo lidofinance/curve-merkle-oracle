d2b2210293eb1e778e8032e4d080180569266e02
Edward Amor <edward.amor3@gmail.com>
2022-02-28 08:32:33 +0000

    feat: add submit_state function

--- contracts/VotingEscrowStateOracle.sol
+++ contracts/VotingEscrowStateOracle.sol
@@ -1,11 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.12;
 
+import {RLPReader} from "./RLPReader.sol";
+import {StateProofVerifier as Verifier} from "./StateProofVerifier.sol";
+
 interface AnyCallProxy {
     function context() external view returns(address, uint256);
 }
 
 contract VotingEscrowOracle {
+    using RLPReader for bytes;
+    using RLPReader for RLPReader.RLPItem;
+
     struct LockedBalance {
         int128 amount;
         uint256 end;
@@ -20,10 +26,18 @@ contract VotingEscrowOracle {
 
     /// Address of the voting escrow contract on Ethereum
     address constant VOTING_ESCROW = 0x5f3b5DfEb7B28CDbD7FAba78963EE202a494e2A2;
+    /// Hash of the voting escrow contract address
+    bytes32 constant VOTING_ESCROW_HASH = keccak256(abi.encodePacked(VOTING_ESCROW));
+
+    /// `VotingEscrow.epoch()` storage slot hash
+    bytes32 constant EPOCH_HASH = keccak256(abi.encode(3));
+
     /// Hash of the block header for the Ethereum genesis block
     bytes32 constant GENESIS_BLOCKHASH = 0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3;
     /// Week in seconds
     uint256 constant WEEK = 1 weeks;
+
+
 
     /// Address of the AnyCallProxy for the chain this contract is deployed on
     address public immutable ANYCALL;
@@ -51,6 +65,89 @@ contract VotingEscrowOracle {
         emit TransferOwnership(address(0), msg.sender);
 
         ANYCALL = _anycall;
+    }
+
+    function submit_state(address _user, bytes memory _block_header_rlp, bytes memory _proof_rlp) external {
+        // verify block header
+        Verifier.BlockHeader memory block_header = Verifier.parseBlockHeader(_block_header_rlp);
+        require(block_header.hash != bytes32(0)); // dev: invalid blockhash
+        require(block_header.hash == _eth_blockhash[block_header.number]); // dev: blockhash mismatch
+
+        // convert _proof_rlp into a list of `RLPItem`s
+        RLPReader.RLPItem[] memory proofs = _proof_rlp.toRlpItem().toList();
+        require(proofs.length == 21); // dev: invalid number of proofs
+
+        // 0th proof is the account proof for Voting Escrow contract
+        Verifier.Account memory ve_account = Verifier.extractAccountFromProof(
+            VOTING_ESCROW_HASH, // position of the account is the hash of its address
+            block_header.stateRootHash,
+            proofs[0].toList()
+        );
+        require(ve_account.exists); // dev: Voting Escrow account does not exist
+
+        // 1st proof is the `VotingEscrow.epoch()` storage slot proof
+        Verifier.SlotValue memory slot_epoch = Verifier.extractSlotValueFromProof(
+            EPOCH_HASH,
+            ve_account.storageRoot,
+            proofs[1].toList()
+        );
+        require(slot_epoch.exists);
+
+        // 2-5th proof are the `VotingEscrow.point_history(uint256)` slots
+        // this is a struct where bias and slope are int128, the position is determined based
+        // on the value of `epoch`
+        Verifier.SlotValue[4] memory slot_point_history;
+        for (uint256 i = 0; i < 4; i++) {
+            slot_point_history[i] = Verifier.extractSlotValueFromProof(
+                keccak256(abi.encode(uint256(keccak256(abi.encode(uint256(keccak256(abi.encode(4))) + slot_epoch.value))) + i)),
+                ve_account.storageRoot,
+                proofs[2 + i].toList()
+            );
+            require(slot_point_history[i].exists); // dev: slot does not exist
+        }
+
+        // 6th proof is the `VotingEscrow.user_point_epoch(address)` slot proof
+        Verifier.SlotValue memory slot_user_point_epoch = Verifier.extractSlotValueFromProof(
+            keccak256(abi.encode(keccak256(abi.encode(6, _user)))),
+            ve_account.storageRoot,
+            proofs[6].toList()
+        );
+        require(slot_user_point_epoch.exists); // dev: slot does not exist
+
+        // 7-10th proof are for `VotingEscrow.user_point_history` slots
+        // similar to `point_history` this is a struct
+        Verifier.SlotValue[4] memory slot_user_point_history;
+        for (uint256 i = 0; i < 4; i++) {
+            slot_user_point_history[i] = Verifier.extractSlotValueFromProof(
+                keccak256(abi.encode(uint256(keccak256(abi.encode(uint256(keccak256(abi.encode(keccak256(abi.encode(5, _user))))) + slot_user_point_epoch.value))) + i)),
+                ve_account.storageRoot,
+                proofs[7 + i].toList()
+            );
+            require(slot_user_point_history[i].exists); // dev: slot does not exist
+        }
+
+        // 11-12th proof are for `VotingEscrow.locked()` this is a struct with 2 members
+        Verifier.SlotValue[2] memory slot_locked;
+        for (uint256 i = 0; i < 2; i++) {
+            slot_locked[i] = Verifier.extractSlotValueFromProof(
+                keccak256(abi.encode(uint256(keccak256(abi.encode(keccak256(abi.encode(2, _user))))) + i)),
+                ve_account.storageRoot,
+                proofs[11 + i].toList()
+            );
+            require(slot_locked[i].exists); // dev: slot does not exist
+        }
+
+        // Remaining proofs are for 2 months worth of slope changes
+        // starting from the week beginning the last global point
+        Verifier.SlotValue[8] memory slot_slope_changes;
+        for (uint256 i = 0; i < 8; i++) {
+            slot_slope_changes[i] = Verifier.extractSlotValueFromProof(
+                keccak256(abi.encode(keccak256(abi.encode(7, (slot_point_history[2].value / WEEK) * WEEK + WEEK * i)))),
+                ve_account.storageRoot,
+                proofs[13 + i].toList()
+            );
+            require(slot_slope_changes[i].exists); // dev: slot does not exist
+        }
     }
 
     /**
